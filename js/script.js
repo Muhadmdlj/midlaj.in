cf001a5a646809fedadb99615f8dc008652595c6
Muhadmdlj <155664111+Muhadmdlj@users.noreply.github.com>
2024-01-17 11:20:44 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
         responsiveClass: true,
         responsive: {
             0: {
-                items: 2,
+                items: 1,
                 nav: true,
                 dots: true,
             },
